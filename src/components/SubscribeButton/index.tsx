efb5be8b6fb84c490283bb26424a467474996153
Gontran Bezerra <gontranbezerra@gmail.com>
2021-03-25 15:47:01 -0300

    feat: Consuming Stripe API (SSR)

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,12 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton(props: SubscribeButtonProps) {
+  const { priceId } = props;
+
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { GetServerSideProps } from 'next';
+
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton/index';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProduct {
+  product: {
+    priceId: string;
+    amount: string;
+  };
+}
+
+export default function Home(props: HomeProduct) {
+  const { product } = props;
   return (
     <>
       <Head>
@@ -18,9 +29,9 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -28,3 +39,25 @@ export default function Home() {
     </>
   );
 }
+
+// função padrão para rodar no Nodejs dentro do Next no lado servidor
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const price = await stripe.prices.retrieve('price_1IYuZMKL9ACPD24bzlYMDh9U', {
+  //   expand: ['product'], // pra trazer mais informaões
+  // });
+  const price = await stripe.prices.retrieve('price_1IYuZMKL9ACPD24bzlYMDh9U');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
