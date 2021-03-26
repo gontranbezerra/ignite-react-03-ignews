b140af103a7fd6e6a7c3ad453eeaa13a5f222b53
Gontran Bezerra <gontranbezerra@gmail.com>
2021-03-25 22:40:38 -0300

    feat: Autentication with NextAuth on GitHub

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
