157d6ea1bb792ecdecaea84ce40325f0b01e0bab
Gontran Bezerra <gontranbezerra@gmail.com>
2021-03-27 19:14:07 -0300

    feat: Payments on Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { useSession, signIn } from 'next-auth/client';
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -6,9 +11,34 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton(props: SubscribeButtonProps) {
   const { priceId } = props;
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    // Criação da checkout session
+    // Onde podemos utilizar as credenciais secretas?
+    // getServerSideProps (SSR)
+    // getStaticProps (SSG)
+    // API Routes
+    try {
+      console.log('Fazendo o subscribe...');
+      const response = await api.post('/subscribe');
+      console.log('response.data', response.data);
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
 
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
       Subscribe now
     </button>
   );
