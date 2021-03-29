1fecbc13539090ff182db779a51664035927af94
Gontran Bezerra <gontranbezerra@gmail.com>
2021-03-29 20:48:33 -0300

    feat: Working with CMS

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,23 @@
+import { cloneElement, ReactElement } from 'react';
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink(props: ActiveLinkProps) {
+  const { children, activeClassName, ...rest } = props;
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : ';';
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,28 @@
+// import { useRouter } from 'next/router';
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton/index';
 
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+  // const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Post</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            {/* <a className={asPath === '/' ? styles.active : ''}>Home</a> */}
+            <a>Home</a>
+          </ActiveLink>
+          {/* <Link href="/posts" prefetch> // prefetch precarregas as páginas */}
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            {/* <a className={asPath === '/posts' ? styles.active : ''}>Posts</a> */}
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
